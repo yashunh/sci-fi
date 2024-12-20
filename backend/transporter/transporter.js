55e6d1eb28c3e376b224b9f46620ddc335694027
yashunh <priyanshuntak@gmail.com>
2024-12-20 11:35:50 +0530

    remove transporter2

--- backend/transporter/transporter.js
+++
@@ -1,9 +0,0 @@
-import nodemailer from "nodemailer"
-
-export const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'scifiinnovationclub@gmail.com',
-        pass: "qylt xqjf tzyv lhbp"
-    }
-});
